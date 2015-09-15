13f3902c3a605b63d9d40baa6bfa86754c80d25d
Clarissa Littler <clarissa.littler@gmail.com>
2015-09-14 18:46:23 -0700

    things are SHAPING up, eh? Eh? Eh........

--- Shapes.hs
+++ Shapes.hs
@@ -8,7 +8,9 @@ import Control.Monad.ST
 
 type Height = Int
 type Width = Int
+type FPoint = (Float,Float)
 type Point = (Int,Int)
+type Thickness = Int
 
 type Color = (Int,Int,Int)
 
@@ -30,12 +32,35 @@ type Color = (Int,Int,Int)
 
 -}
 
-data Shape = Ellipse Int Int Point Point Thickness
-           | Arc Int Point Float Thickness 
-           | Line Point Point Thickness -- <-- ^ relative points 
+{- 
+   Okay, note to self, what we need to do is have a scale of (-1,1) on the x and y-axis for the internal representation of the shapes and then we can map it onto actual regions later with proper scaling. Will this work? I don't know but it seems reasonable to someone who hasn't thought very long about this. 
+
+   Now, you might ask "why have this intermmediate DSL to begin with?" ehh basically because I want to do cute things with random instances and that sort of thing
+-}
+
+data Shape = Ellipse Int Int FPoint FPoint Thickness
+           | Arc Int FPoint Float Thickness 
+           | Line FPoint FPoint Thickness -- <-- ^ relative points 
            | Vertical Shape Shape
            | Horizontal Shape Shape
            | On Shape Shape -- layer shapes onto one region
            | Region Float Float Shape -- scaling another shape to fit a region and giving it a background color
            deriving (Eq,Show)
 
+{- 
+   Rendering is going to take 
+   1. a shape
+   2. the actual marray as well as
+   3. the current bounds of the region (initially the same as the whole array)
+
+   and of course it'll return the array in the ST monad
+-}
+
+render :: Shape -> STArray s (Int,Int) Color -> (Int,Int,Int,Int) -> ST s (STArray s (Int, Int) Color)
+render (Region xscale yscale s) a (x1,x2,y1,y2) = render s a (xmid - xsize, xmid + xsize,
+                                                              ymid - ysize, ymid + ysize)
+    where xsize = round $ (fromIntegral $ (abs (x2 - x1)) `div` 2) * xscale 
+          ysize = round $ (fromIntegral $ (abs (y2 - y1)) `div` 2) * yscale
+          xmid = (x1 + x2) `div` 2
+          ymid = (y1 + y2) `div` 2
+           
